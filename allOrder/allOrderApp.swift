bb06d65c3d45f87221226018ba3b741f094d076d
SanekWhite <bro.b98@mail.ru>
2024-12-10 20:26:27 +0700

    Initial Commit

---
+++ allOrder/allOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  allOrderApp.swift
+//  allOrder
+//
+//  Created by Александр Белый on 10.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct allOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
